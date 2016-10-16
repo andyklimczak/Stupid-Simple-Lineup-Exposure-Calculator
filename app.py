b0dd32a47f6b6e99c5b9e87b6dde69346f04202d
Andy Klimczak <andyklimczak@fastmail.com>
2016-10-16 18:27:51 -0400

    Start parsing QB

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import os, json
+import os, json, csv, io
+import pandas as pd
+from collections import defaultdict
 from flask import Flask, url_for, request, redirect, send_from_directory, session, render_template
 
 ALLOWED_EXTENSIONS = set(['csv'])
@@ -6,39 +8,39 @@ ALLOWED_EXTENSIONS = set(['csv'])
 app = Flask(__name__)
 app.secret_key = '123'
 
+def calculate(data):
+    print(type(data).__name__)
+    dict = defaultdict(int)
+    for player in data:
+        dict[player] += 1
+    return dict
+
 def parse(file):
     dict = {}
-    dict['m'] = {}
-    dict['m']['andy'] = 4
-    dict['f'] = {}
-    dict['f']['jae'] = 5
-    dict['f']['lau'] = 5
+    with io.StringIO(file.stream.read().decode("UTF8"), newline=None) as csv_file:
+        data = pd.read_csv(csv_file)
+        dict['QB'] = calculate(data['QB'])
+        dict['FLEX'] = calculate(pd.concat([data['WR'], data['RB']]))
     return json.dumps(dict)
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # if 'file' not in request.files:
-        # flash('No file part')
-        # return redirect(request.url)
-        # file = request.files['file']
-        # if file.filename == '':
-        # flash('No selected file')
-        # return redirect(request.url)
-        # if file and allowed_file(file.filename):
-        # session['data'] = parse(file)
-        file = None
-        session['data'] = parse(file)
-        return redirect(url_for('done'))
-    return '''
-<!doctype>
-<title>Upload new File</title>
-<h1>Upload new File</h1>
-<form action="" method=post enctype=multipart/form-data>
-<p><input type=file name=file>
-<input type=submit value=Upload>
-</form>
-'''
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            session['data'] = parse(file)
+            return redirect(url_for('done'))
+    return render_template("index.html")
 
 @app.route('/upload')
 def done():
